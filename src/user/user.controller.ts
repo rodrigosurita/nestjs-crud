ba9d09484d6dda57e23a8c53c5d638b3ea223502
Rodrigo Surita <rodrigosurita@live.com>
2021-05-07 19:29:57 -0300

    getUserByName added to the api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { User } from "./user.entity";
 import { UserService } from "./user.service";
 
@@ -11,6 +11,12 @@ export class UserController {
     public create(@Body() user:User):User{
         const userCreated = this.userService.create(user);
         return userCreated;
+    }
+
+    @Get(':userName')
+    public getUserByName(@Param('userName') userName: string) {
+        const userFounded = this.userService.getUserByName(userName);
+        return userFounded;
     }
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,9 +3,22 @@ import { User } from "./user.entity";
 
 @Injectable()
 export class UserService {
-    private users = [];
+    private users:Array<User> = [{
+        id: 1,
+        name:  'Rodrigo',
+        email: 'rodrigosurita@live.com',
+        password: '123456',
+        fullName: 'Rodrigo Surita da Silva',
+        registerDate: new Date()
+    }];
+
     public create(user: User): User{
         this.users.push(user);
         return user;
     }
+
+    public getUserByName(userName: string):User {
+        return this.users.find(user => user.name == userName);
+    }
+
 }
